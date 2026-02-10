c86f071262c65cda53622c978bfe6d77232e887c
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2026-02-10 11:27:47 +0100

    refactor: convert files to ts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router"
 import "./Header.css"
 
-export default function Header({ cart }) {
+type HeaderProps = {
+  cart: {
+    productId: string
+    quantity: number
+    deliveryOptionId: string
+  }[]
+}
+
+export default function Header({ cart }: HeaderProps) {
   let totalQuantity = 0
 
   cart.forEach((cartItem) => {
